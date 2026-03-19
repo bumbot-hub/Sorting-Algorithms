7880363fdc7dd9e546686e5162f1578be85b374f
bumbot-hub <kkamil.kula@gmail.com>
2026-03-19 16:44:42 +0100

    chore: ArrayGenerator class implemented

--- include/ArrayGenerator.hpp
+++ include/ArrayGenerator.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <cstdlib>
 #include <type_traits>
+#include <fstream>
+#include <string>
+#include <iostream>
 
 template <typename T>
 class ArrayGenerator {
@@ -8,13 +11,9 @@ public:
     static T* GenerateRandom(int size, T minValue, T maxValue) {
         T* array = new T[size];
         for (int i = 0; i < size; ++i) {
-            if constexpr (std::is_integral_v<T>) {
-                array[i] = minValue + rand() % (maxValue - minValue + 1);
-            }
-            else if constexpr (std::is_floating_point_v<T>) {
-                array[i] = minValue + (maxValue - minValue) * (rand() / (double)RAND_MAX);
-            }
+            array[i] = RandomValue(minValue, maxValue);
         }
+
         return array;
     }
 
@@ -23,6 +22,7 @@ public:
         for (int i = 0; i < size; ++i) {
             array[i] = i;
         }
+
         return array;
     }
 
@@ -31,6 +31,71 @@ public:
         for (int i = 0; i < size; ++i) {
             array[i] = size - i;
         }
+
         return array;
+    }
+
+    // Comment how partially sorted array works
+    static T* Generate33Sorted(int size, T maxValue) {
+        T* array = new T[size];
+        int sortedSize = size * 0.33;
+
+        for (int i=0; i<size; i++) {
+            if (i<sortedSize) {
+                array[i] = 0;
+            }else {
+                array[i] = RandomValue(1, maxValue);
+            }
+        }
+
+        return array;
+    }
+
+    static T* Generate66Sorted(int size, T maxValue) {
+        T* array = new T[size];
+        int sortedSize = size * 0.66;
+
+        for (int i=0; i<size; i++) {
+            if (i<sortedSize) {
+                array[i] = 0;
+            }else {
+                array[i] = RandomValue(1, maxValue);
+            }
+        }
+
+        return array;
+    }
+
+    static T* UploadFromFile(const std::string& fileName, int size) {
+        T* array = new T[size];
+        std::fstream file(fileName, std::ios::in);
+
+        if (!file.is_open()) {
+            std::cout<<"Could not open file "<<fileName<<std::endl;
+            delete [] array;
+
+            return nullptr;
+        }
+
+        for (int i=0; i<size; i++) {
+            if (!file >> array[i])
+            {
+                std::cout<<"File does not have enough data"<<std::endl;
+                delete[] array;
+
+                return nullptr;
+            }
+        }
+
+        return array;
+    }
+
+private:
+    static T RandomValue(T minValue, T maxValue) {
+        if constexpr (std::is_integral_v<T>) {
+            return minValue + rand() % (maxValue - minValue + 1);
+        } else if constexpr (std::is_floating_point_v<T>) {
+            return minValue + (maxValue - minValue) * (rand() / (double)RAND_MAX);
+        }
     }
 };
